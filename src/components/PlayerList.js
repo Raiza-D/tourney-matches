3c3adc6116440a633ef2acdc27325ce9ed02c467
Raiza De Guzman <santos_dg7@hotmail.com>
2022-07-06 15:39:33 -0700

    Pass data for an individual player into component

--- src/components/PlayerList.js
+++ src/components/PlayerList.js
@@ -8,11 +8,18 @@ import Player from './Player';
 function PlayerList(props) {
   const playerDataArray = preparePlayerData(playerData);
   const parsedPlayerData = addWinsToPlayers(playerDataArray, matchData);
-  
+
+  const onePlayer = parsedPlayerData[0];
+
   return (
     <section className="PlayerList">
       <h1>Current participating players</h1>
-      <Player />
+      <Player
+        gamerTag={onePlayer.gamerTag}
+        firstname={onePlayer.firstName}
+        lastName={onePlayer.lastName}
+        wins={onePlayer.wins}
+      />
     </section>
   );
 };
