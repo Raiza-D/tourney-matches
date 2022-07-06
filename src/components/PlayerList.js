46f77b8d8fac855f905487ea73da988d3283a67b
Raiza De Guzman <santos_dg7@hotmail.com>
2022-07-06 15:03:25 -0700

    Import helper functions

--- src/components/PlayerList.js
+++ src/components/PlayerList.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import playerData from '../data/playerData';
 import matchData from '../data/matchData';
+import { preparePlayerData, addWinsToPlayers } from '../helpers/playerHelpers';
 import Player from './Player';
+
 
 function PlayerList(props) {
   return (
