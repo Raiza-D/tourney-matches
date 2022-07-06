0a77e7d7a0b7a6f21f9ba1c579fdd9563ca53d75
Raiza De Guzman <santos_dg7@hotmail.com>
2022-07-06 14:59:34 -0700

    Add comments on top of each function

--- src/helpers/playerHelpers.js
+++ src/helpers/playerHelpers.js
@@ -1,15 +1,19 @@
-
-
+// Converts playerData object into array
 export const preparePlayerData = (playerData) => {
   return Object.values(playerData);
 };
 
+
+/* Calculates match wins for each player.
+Iterates over each player in playerData. Then iterates over each match in matchData.
+If player in iteration is the winner of the match, increase tally.
+Return player showing wins key and value. */
 export const addWinsToPlayers = (playerData, matchData) => {
   return playerDataArray.map((player) => {
 
     // Calculating the number of wins in the matchData
     const currentWins = matchData.reduce((accumulator, match) => {
-      
+
       // Adds a win if the gamerTag matches
       if (match.winner === player.gamerTag) {
         return (accumulator += 1);
