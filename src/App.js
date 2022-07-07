a8295019e070fae5e17d0e5be77b7eb0ef8acfa5
Raiza De Guzman <santos_dg7@hotmail.com>
2022-07-06 17:52:04 -0700

    Import playerData and pass it into PlayerList as a prop

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import matchData from './data/matchData';
+import playerData from './data/playerData';
 import PlayerList from './components/PlayerList';
 import MatchList from './components/MatchList';
 
@@ -10,7 +11,7 @@ function App(props) {
         Tourney Matches{" "}
         <span>Where Coding and Tournaments found their Match!</span>
       </h1>
-      <PlayerList matchData={matchData}/>
+      <PlayerList matchData={matchData} playerData={playerData}/>
       <MatchList matchData={matchData}/>
     </div>
   );
