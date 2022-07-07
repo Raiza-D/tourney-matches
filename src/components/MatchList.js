86ca93c47e96d37495e87573692b39fa253b269a
Raiza De Guzman <santos_dg7@hotmail.com>
2022-07-06 17:09:07 -0700

    Remove import line to import matchData. Change oneMatch var to accept matchData as a prop.

--- src/components/MatchList.js
+++ src/components/MatchList.js
@@ -1,9 +1,8 @@
 import React from 'react';
-import matchData from '../data/matchData';
 import Match from './Match';
 
 function MatchList(props) {
-  const oneMatch = matchData[0];
+  const oneMatch = props.matchData[0];
 
   return (
     <section className="PlayerList MatchList">
